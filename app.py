cf2fcaed2f26b334dd576c3a38bce166e86620e3
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-10-29 16:36:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,21 +28,18 @@ st.write("""
 #Getting the Data
 
 df = pd.read_csv("stroketrain.csv")
-
+df_data = pd.DataFrame(data)
 st.dataframe(df)
 #remove the rows with missing values
-df = df.dropna( subset= ['bmi'])
 df = df.drop(['id'], axis=1)
 
-from sklearn.preprocessing import LabelEncoder
-labelencoder = LabelEncoder()
+df_data = df_data.fillna(df_data['bmi'].median())
 
-df.iloc[:,0] = labelencoder.fit_transform(df.iloc[:,0].values)
-df.iloc[:,4] = labelencoder.fit_transform(df.iloc[:,4].values)
-df.iloc[:,5] = labelencoder.fit_transform(df.iloc[:,5].values)
-df.iloc[:,6] = labelencoder.fit_transform(df.iloc[:,6].values)
-df.iloc[:,9] = labelencoder.fit_transform(df.iloc[:,9].values)
-
+for i in range(df_data.shape[1]):
+    if df_data.iloc[:,i].dtypes == object:
+        lbl = LabelEncoder()
+        lbl.fit(list(df_data.iloc[:,i].values))
+        df_data.iloc[:,i] = lbl.transform(list(df_data.iloc[:,i].values))
 
 #setting a subheader
 st.subheader('Data Information: ')
