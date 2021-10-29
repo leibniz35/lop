b7bfb3d731398465ac11a99457353825d65a4c31
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-10-29 16:41:23 +0300

    Update nefa.py

--- nefa.py
+++ nefa.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 import streamlit as st
+from urllib.request import urlretrieve
+
 
 st.write("""
 #Stroke Detection
