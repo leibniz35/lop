28bcc8503fc03791eb4756fe408e8bef8534cb10
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-10-29 15:19:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
+from pathlib import Path
 
 path = Path()
 
