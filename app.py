3446fa387fc088d2734a3fa329dbe97afe437567
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-10-29 15:16:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-form sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
 
