fb3fd7961d43c9a69d753d094ebd51b32c9e196d
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-10-29 19:48:45 +0300

    Update nefa.py

--- nefa.py
+++ nefa.py
@@ -66,7 +66,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20 , rando
 #Getting input from the user
 def get_user_input():
     list1 = ['0', '1', '2']
-    st.sidebar.slider("0 = Female, 1 = Male, 2 = Other") #
+    st.sidebar.write("0 = Female, 1 = Male, 2 = Other") #
     gender = st.sidebar.selectbox('Gender', list1)
     age = st.sidebar.slider('age', 0, 100, 0)
     list2 = ['0', '1']
